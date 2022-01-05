b69a296a16594e2b67cd93c67a8aa6abe3ab1bfb
Ковальчук Алексей <kovalchuk.aleksey@outlook.com>
2022-01-05 22:28:42 +0300

    Add getProducts on Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit{
   }
 
   public ngOnInit(): void {
-    // this._getProducts();
+    this._getProducts();
   }
 
   public deleteProduct(id: string): void {
